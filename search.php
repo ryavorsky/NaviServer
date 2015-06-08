e72413a0523402c2e463f34fce1a7d7729ec2fd1
ryavorsky <ryavorsky@gmail.com>
2015-06-08 13:46:24 +0300

    Search Unix->Dos

---
+++ search.php
@@ -0,0 +1,27 @@
+﻿<?php 
+
+	$navi_city = $_GET['city'];
+	if ($navi_city == "1415"){ 
+		$data = file('NaviDB_SanFran.csv');
+		$city = "San Francisco";
+	} elseif ($navi_city == "7495"){ 
+		$data = file('NaviDB_Msk.csv');
+		$city = "Москва";
+	} elseif ($navi_city == "458"){ 
+		$data = file('NaviDB_Aarhus.csv');
+		$city = "Århus";
+	} else {
+		echo 'Covered cities: <br/><br/>Moscow (7495), <br/>San Francisco (1415), <br/>Aarhus (458)';
+		exit();
+	};
+	
+	$navi_addr = $_GET['addr'];
+	
+	foreach ($data as $addr){
+		if (strpos($addr, $navi_addr) === 0) {
+			$pair = explode("\t", $addr);
+			echo $navi_city."\t@".$pair[0].":\t".$city.",\t".$pair[1]."\t<br/>";
+		};
+	};
+?>
+    
